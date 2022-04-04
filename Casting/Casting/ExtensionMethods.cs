f21ddd80633b92134c4dd317c58344a4c6aaa9b8
AliVeli7 <6gmrj9e@code.edu.az>
2022-04-05 03:22:30 +0400

    Created Hometask File

---
+++ Casting/Casting/ExtensionMethods.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Casting
+{
+    public static class ExtensionMethods
+    {
+
+        public static bool CustomContains(this string str, string word)
+        {
+            if (str.Contains(word))
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public static bool CustomContains(this string str ,char element)
+        {
+            if (str.Contains(element))
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public static bool IsPrime(this int num)
+        {
+            int i;
+            for (i = 2; i <= num - 1; i++)
+            {
+                if (num % i == 0)
+                {
+                    return false;
+                }
+            }
+            if (i == num)
+            {
+                return true;
+            }
+            return false;
+
+
+        }
+    }
+}
---
+++ Casting/Casting/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Casting
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            int a = 31;
+            Console.WriteLine(a.IsPrime());
+            string word = "hello HoW Are you?";
+            
+            Console.WriteLine(word.CustomContains("hello"));
+            Console.WriteLine(word.CustomContains('s'));
+
+
+
+
+
+
+        }
+    }
+}
